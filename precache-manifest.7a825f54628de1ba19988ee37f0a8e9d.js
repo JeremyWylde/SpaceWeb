e284f5979e85bb333b0438ac5c03492e9fa4f999
JeremyWylde <63048608+JeremyWylde@users.noreply.github.com>
2020-09-21 13:06:20 +0300

    Updates

---
+++ precache-manifest.7a825f54628de1ba19988ee37f0a8e9d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e761c0186d2f8e9709779fddbce25c3d",
+    "url": "/spaceWeb/index.html"
+  },
+  {
+    "revision": "acc1aa3df16076bd5f92",
+    "url": "/spaceWeb/static/css/main.37d09096.chunk.css"
+  },
+  {
+    "revision": "1c7c6414c33fba2bbf9f",
+    "url": "/spaceWeb/static/js/2.ffc821de.chunk.js"
+  },
+  {
+    "revision": "27ca4020711bd21574faae3da7dd14e9",
+    "url": "/spaceWeb/static/js/2.ffc821de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc1aa3df16076bd5f92",
+    "url": "/spaceWeb/static/js/main.d252b4a3.chunk.js"
+  },
+  {
+    "revision": "7c90c65fa1ca2b3f2b96",
+    "url": "/spaceWeb/static/js/runtime-main.65c641df.js"
+  },
+  {
+    "revision": "74b1dbabdec6f58026001f88a5259334",
+    "url": "/spaceWeb/static/media/VPS.74b1dbab.svg"
+  },
+  {
+    "revision": "3406f2761b94d59c45d29d28c6438561",
+    "url": "/spaceWeb/static/media/account.3406f276.svg"
+  },
+  {
+    "revision": "8d7dd1edb9a5fefa86783a6ad352c476",
+    "url": "/spaceWeb/static/media/alert.8d7dd1ed.svg"
+  },
+  {
+    "revision": "e8e65b2df7847769644fd17c26620084",
+    "url": "/spaceWeb/static/media/bortovoyZhurnal.e8e65b2d.svg"
+  },
+  {
+    "revision": "6ba16b949fb639fd99552f031361479a",
+    "url": "/spaceWeb/static/media/domens.6ba16b94.svg"
+  },
+  {
+    "revision": "48632028c7c10e7488ec9d456f334c1e",
+    "url": "/spaceWeb/static/media/sideBarLogo.48632028.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SpaceWeb/precache-manifest.891da470f76b9040bacf5c762f657d47.js"
+  "/spaceWeb/precache-manifest.7a825f54628de1ba19988ee37f0a8e9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SpaceWeb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spaceWeb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
